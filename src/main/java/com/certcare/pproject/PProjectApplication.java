96acc115752f4664d6ffafcf0fafecaf911d2afb
suran28 <chocomilk2638@gmail.com>
2023-11-18 11:12:16 +0900

    create project

---
+++ src/main/java/com/certcare/pproject/PProjectApplication.java
@@ -0,0 +1,13 @@
+package com.certcare.pproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/certcare/pproject/dto/TokenDto.java
@@ -0,0 +1,16 @@
+package com.certcare.pproject.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class TokenDto {
+    private Long uid;
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private Long accessTokenExpiresIn;
+}
