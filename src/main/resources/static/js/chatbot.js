eb9e26818a3eb0682fd2a446b58b61cf361a923c
heejinny <heejin.010702@gmail.com>
2023-12-10 21:18:26 +0900

    chat Complete

--- src/main/resources/static/js/chatbot.js
+++ src/main/resources/static/js/chatbot.js
@@ -134,27 +134,27 @@ function requestAi(message) {
         // .then(res => res.text())
         // .then(data => {
         //     console.log(data);
-
+        //
         //     addAiChatToMemory(data, "chatSave")
         // })
         // .catch(error => {
         //     console.error('Error:', error);
-        // });
+        // })
 
         .then(res => res.json())
         .then(res => {
             console.log(res)
 
-
-            // if (res.choices[0].message.content.includes('"message"')) {
+            if (res.status === 500) {
+                console.log("챗봇 응답:", res);
+                let assistantResponse = "죄송합니다. 챗봇 답변을 불러오는데 실패했습니다."
+                addAiChatToMemory(assistantResponse, "chatSave")
+            } else {
+                console.log("챗봇 응답:", res);
+                console.log("챗봇 응답:", res.choices[0].message.content);
                 let assistantResponse = res.choices[0].message.content
-            // } else {
-            //     assistantResponse = JSON.parse(res.choices[0].message.content).botResponse;
-            // }
-            console.log(assistantResponse)
-
-
-            addAiChatToMemory(assistantResponse, "chatSave")
+                addAiChatToMemory(assistantResponse, "chatSave")
+            }
         })
         .catch(error => {
                 console.error('Error:', error);
