dd75ad9dc7a4944307df15755d97317238db52e6
suran28 <chocomilk2638@gmail.com>
2023-12-10 22:56:44 +0900

    update excel file path

--- src/main/java/com/certcare/pproject/PProjectApplication.java
+++ src/main/java/com/certcare/pproject/PProjectApplication.java
@@ -1,6 +1,7 @@
 package com.certcare.pproject;
 
 import com.certcare.pproject.service.ExcelService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 import java.io.IOException;
-
+@Slf4j
 @SpringBootApplication
 public class PProjectApplication implements CommandLineRunner {
 
@@ -21,9 +22,28 @@ public class PProjectApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws IOException {
 		File currentDirectory = new File(System.getProperty("user.dir"));
-		String target = "data&AI" + File.separator + "data" + File.separator + "cert-info.xlsx";
-		File targetDirectory = new File(currentDirectory, target);
-		String filePath = String.valueOf(targetDirectory);
-		excelService.readExcelFileAndToEntity(filePath);
+
+		// 배포용
+		File parentDirectory = currentDirectory.getParentFile();
+		if (parentDirectory != null) {
+			parentDirectory = parentDirectory.getParentFile();
+			if (parentDirectory != null) {
+				String target = "data&AI" + File.separator + "data" + File.separator + "cert-info.xlsx";
+				File targetDirectory = new File(parentDirectory, target);
+				String filePath = String.valueOf(targetDirectory);
+				excelService.readExcelFileAndToEntity(filePath);
+			} else {
+				log.info("상위 디렉토리가 없습니다.");
+			}
+		} else {
+			log.info("상위 디렉토리가 없습니다.");
+		}
+
+		// 로컬용
+//		String target = "data&AI" + File.separator + "data" + File.separator + "cert-info.xlsx";
+//		File targetDirectory = new File(currentDirectory, target);
+//		String filePath = String.valueOf(targetDirectory);
+//		excelService.readExcelFileAndToEntity(filePath);
+
 	}
 }
