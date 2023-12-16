c656879bc4d9be377c84a3a14e4ba815397169a7
heejinny <heejin.010702@gmail.com>
2023-12-16 20:59:26 +0900

    admin login

---
+++ src/main/resources/static/js/adminLogin.js
@@ -0,0 +1,45 @@
+function adminLoginForm() {
+    const adminId = document.getElementById('adminId').value
+    const password = document.getElementById('adminPw').value
+
+    console.log(adminId, password)
+
+    // 로그인 요청 보내기
+    fetch('/admin/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                userId: adminId,
+                password: password
+            }
+        )
+    })
+        .then(res => res.json())
+        .then(res => {
+            const accessToken = res.accessToken;
+            const username =res.username
+
+            console.log(res)
+
+            if (username === 'admin') {
+                setCookie('accessToken', accessToken, 7);
+                setCookie('username', username, 7)
+
+                alert("로그인이 완료되었습니다.")
+
+                window.location.href = "/admin/members"
+            } else {
+                alert("아이디 및 비밀번호를 다시 확인해주세요.");
+                return null;
+            }
+        })
+}
+
+function setCookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
