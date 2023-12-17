4b00b785b7f24297cd1e1d5c70e975bef44a8f94
heejinny <heejin.010702@gmail.com>
2023-12-17 19:59:35 +0900

    login alert

--- src/main/resources/static/js/chatbot.js
+++ src/main/resources/static/js/chatbot.js
@@ -219,7 +219,7 @@ function dissatiBtn() {
                         alert("반영이 완료되었습니다.");
                     }
                     else if(res.status === 500){
-                        alert("반영이 되지 않았습니다 다시 한 번 눌러주시기 바랍니다.");
+                        alert("로그인 후 이용해주시기 바랍니다.");
                     }
                 })
                 .catch(error => {
